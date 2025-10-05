178d3a863a625f3b30edea0d6cd41d42c3c4d667
Dan <gvozdevdd@gmail.com>
2025-10-05 11:57:03 +0300

    formated

--- site_generator/src/main.py
+++ site_generator/src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 
+
 def main():
     print(TextNode("some test text", TextType.LINK, "https://www.google.com"))
+
 
 if __name__ == "__main__":
     main()
--- site_generator/src/textnode.py
+++ site_generator/src/textnode.py
@@ -2,6 +2,7 @@ from __future__ import annotations  # noqa: F404
 
 from enum import Enum
 
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -10,18 +11,19 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
     def __init__(self, text: str, text_type: TextType, url: str = None):
         self.text = text
         self.text_type = text_type
         self.url = url
-    
+
     def __eq__(self, value: TextNode) -> bool:
         return (
             self.text_type == value.text_type
             and self.text == value.text
             and self.url == value.url
         )
-    
+
     def __repr__(self) -> str:
-        return(f"TextNode({self.text}, {self.text_type.value}, {self.url})")
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
